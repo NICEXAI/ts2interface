8b97b56aada09848e602803cd98f01fb7ee88e63
NICEXAI <1500527791@qq.com>
2022-06-15 17:03:26 +0800

    chore: add > ts2interface@1.0.5 publish > npm run build && npm publish
    > ts2interface@1.0.5 build
    > rm -rf dist && tsc command

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { object2interface } from "./object"
+import { json2interface } from "./json"
+import { yaml2interface } from "./yaml"
 
-export { object2interface }
+export { object2interface, json2interface, yaml2interface }
